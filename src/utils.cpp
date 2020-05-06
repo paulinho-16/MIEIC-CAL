50c8e83f7e1f1e8927b836854ae717b90c9ceade
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2020-05-06 12:45:14 +0100

    Added utils files

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,17 @@
 #include "utils.h"
+
+void readMap(string directory) {
+
+}
+
+void readNodes(string file_directory) {
+
+}
+
+void readEdges(string file_directory) {
+
+}
+
+void readTags(string file_directory) {
+
+}
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,14 @@
 #ifndef CAL_FP05_UTILS_H
 #define CAL_FP05_UTILS_H
 
+#include <iostream>
 
+using namespace std;
+
+void readMap();
+
+void readNodes();
+void readEdges();
+void readTags();
 
 #endif //CAL_FP05_UTILS_H
